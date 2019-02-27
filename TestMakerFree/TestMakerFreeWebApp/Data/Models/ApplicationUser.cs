0e459a1ece9963c96bc0bba81b92970cce86c604
Sarah Luo <sarahl@palittoconsulting.com>
2019-02-27 10:50:39 -0500

    Add User and Quiz data model classes

---
+++ TestMakerFree/TestMakerFreeWebApp/Data/Models/ApplicationUser.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TestMakerFreeWebApp.Data.Models
+{
+    public class ApplicationUser
+    {
+        #region constructor
+        public ApplicationUser()
+        {
+
+        }
+        #endregion
+
+        #region Properties
+        [Key]
+        [Required]
+        public string Id { get; set; }
+
+        [Required]
+        [MaxLength(128)]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public string Notes { get; set; }
+
+        [Required]
+        public int Type { get; set; }
+
+        [Required]
+        public int Flags { get; set; }
+
+        [Required]
+        public DateTime CreateDate { get; set; }
+
+        [Required]
+        public DateTime LastModifiedDate { get; set; }
+        #endregion
+    }
+}
