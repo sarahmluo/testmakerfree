775c4dbbe5dc1b9b93da6e1b6a5359327a47584e
Sarah Luo <sarahl@palittoconsulting.com>
2019-02-27 11:48:34 -0500

    Add user role enum

---
+++ TestMakerFree/TestMakerFreeWebApp/Enums/Enums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestMakerFreeWebApp.Enums
+{
+    public class Enums
+    {
+        public enum RoleType
+        {
+            Admin = 1,
+            User = 2
+        }
+    }
+}
